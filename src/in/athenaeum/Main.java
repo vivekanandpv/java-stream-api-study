f49c7b01babe8e68a4fed110d0976b10f2d355a7
vivek <vivek@athenaeum.in>
2021-03-15 22:46:22 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -1,17 +1,29 @@
 package in.athenaeum;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> cities = List.of("Mumbai", "mumbai", "Mysuru");
+        List<String> cities = List.of("Ratnagiri", "Dharwad", "Haveri", "Bidar");
+        //  https://docs.oracle.com/en/java/javase/15/docs/api/java.base/java/util/stream/Stream.html#findFirst()
+        //  findAny is a terminal operator that produces Optional<T>
+        //  Optional<T> because result could not exist
+        Optional<String> findAnyResult
+                = cities.stream()
+                .filter(s -> s.length() > 7)
+                .findAny();
 
-        //  distinct is a non-terminal operator
-        //  that applies filtration (remembers the processed items)
-        cities.stream()
-                .map(String::toLowerCase)
-                .distinct()
-                .forEach(System.out::println);
+        Optional<String> findFirstResult
+                = cities.stream()
+                .filter(s -> s.length() > 7)
+                .findFirst();
+
+        System.out.println("findAnyResult: " + findAnyResult);
+        System.out.println("findFirstResult: " + findFirstResult);
+
+        //  findAny is non-deterministic, it can potentially return any element
+        //  Please read the documentation
     }
 }
