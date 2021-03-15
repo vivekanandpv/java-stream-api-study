ebd080c35e6b8056104e254341a950b6344a4dbb
vivek <vivek@athenaeum.in>
2021-03-15 22:58:00 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -6,13 +6,20 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> cities = List.of("Ratnagiri", "Dharwad", "Haveri", "Bidar");
+        List<Integer> numbers = List.of(8, 2, 4, 5, 3, 2, 1, 7, 8);
 
-        //  skip is a non-terminal operator that applies
-        //  filtration with remembrance
+        //  takeWhile is like filter, but stops the pipeline as soon as
+        //  the non-match is found (while filter continues to look for
+        //  other elements)
 
-        cities.stream()
-                .skip(2)   //only Haveri and Bidar
-                .forEach(System.out::println);
+        numbers.stream()
+                .takeWhile(n -> n % 2 == 0) //stops the stream at first non-match
+                .forEach(System.out::println);  //8,2,4
+
+        System.out.println("---------------------------");
+
+        numbers.stream()
+                .filter(n -> n % 2 == 0) //skips non matching elements
+                .forEach(System.out::println);  //8,2,4,2,8
     }
 }
