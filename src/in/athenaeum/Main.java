2fbe38de920ed57742b3de0dca86f54551828e36
vivek <vivek@athenaeum.in>
2021-03-15 22:27:51 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -5,21 +5,27 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	    List<String> cities = List.of("Mumbai", "Chennai", "Mysuru");
+        List<String> cities = List.of("Mumbai", "Chennai", "Mysuru");
 
-	    cities.stream()
-                .map(s -> {
-                    System.out.println("Map1: " + s);
-                   return s.toUpperCase();
-                })
-                .map(s -> {
-                    System.out.println("Map2: " + s);
-                    return s.length();
-                })
-                .filter(i -> {
-                    System.out.println("Filter: " + i);
-                    return i > 6;
-                })
-                .forEach(s -> System.out.println("forEach: " + s));
+        //  https://docs.oracle.com/javase/tutorial/java/javaOO/methodreferences.html
+        //  If there is a static method that has the same signature as the lambda,
+        //  or a method available on the parameter's type,
+        //  we can provide the method reference as Type::method
+        //  Using method references makes the code concise and clean
+        cities.stream()
+                .map(String::toUpperCase)   //toUpperCase is available on String
+                .forEach(Main::printName);  //printName is a static method
+        //  .forEach(Provider::printName);  //also possible
+        //  .forEach(System.out::println);  //also possible
+    }
+
+    public static void printName(String s) {
+        System.out.println(s);
+    }
+}
+
+interface Provider {
+    static void printName(String name) {
+        System.out.println(name);
     }
 }
