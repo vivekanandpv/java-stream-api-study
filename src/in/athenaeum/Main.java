565e6c7a5af6fb8e11d90a1c02b05198d715be6c
vivek <vivek@athenaeum.in>
2021-03-15 17:33:55 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -1,8 +1,25 @@
 package in.athenaeum;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    List<String> cities = List.of("Mumbai", "Chennai", "Mysuru");
+
+	    cities.stream()
+                .map(s -> {
+                    System.out.println("Map1: " + s);
+                   return s.toUpperCase();
+                })
+                .map(s -> {
+                    System.out.println("Map2: " + s);
+                    return s.length();
+                })
+                .filter(i -> {
+                    System.out.println("Filter: " + i);
+                    return i > 6;
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
     }
 }
