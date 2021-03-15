af54ca2fd08927751ad901a186f6e1e963d6c607
vivek <vivek@athenaeum.in>
2021-03-15 22:34:56 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -7,25 +7,12 @@ public class Main {
     public static void main(String[] args) {
         List<String> cities = List.of("Mumbai", "Chennai", "Mysuru");
 
-        //  https://docs.oracle.com/javase/tutorial/java/javaOO/methodreferences.html
-        //  If there is a static method that has the same signature as the lambda,
-        //  or a method available on the parameter's type,
-        //  we can provide the method reference as Type::method
-        //  Using method references makes the code concise and clean
-        cities.stream()
-                .map(String::toUpperCase)   //toUpperCase is available on String
-                .forEach(Main::printName);  //printName is a static method
-        //  .forEach(Provider::printName);  //also possible
-        //  .forEach(System.out::println);  //also possible
-    }
+        //  count is a terminal operator that produces a long result
+        long result =
+                cities.stream()
+                        .filter(s -> s.length() > 6)
+                        .count();   //=1
 
-    public static void printName(String s) {
-        System.out.println(s);
-    }
-}
-
-interface Provider {
-    static void printName(String name) {
-        System.out.println(name);
+        System.out.println(result);
     }
 }
