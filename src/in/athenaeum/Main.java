e0aed55ae71f4be77f0c946aa48845fb8b50d127
vivek <vivek@athenaeum.in>
2021-03-16 11:31:04 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -5,15 +5,47 @@ import java.util.List;
 import java.util.Optional;
 
 public class Main {
+    //  Exercise: company produces cars
+    //  every car has some colors (red, silver, white, blue)
+    //  the company how many total colors are there for all the cars?
 
     public static void main(String[] args) {
-        List<String> names = List.of("Raman", "Einstein", "Rutherford", "Heisenberg", "Bohr");
+
+        List<Car> cars = List.of(
+                new Car("Camry", new String[]{"Blue", "White", "Black"}),
+                new Car("Etios", new String[]{"Silver", "Brown", "White"}),
+                new Car("Corolla", new String[]{"Golden", "Black", "White"})
+        );
 
         //  flatMap applies map to inner collection
         //  it's a map that returns another stream explicitly
         //  Arrays.stream() is a convenience method
-        names.stream()
-                .flatMap(name -> Arrays.stream(name.split("[aeiou]")))
-                .forEach(System.out::println);
+
+        long totalColours =
+                cars.stream()
+                        .flatMap(c -> Arrays.stream(c.getColours()))
+                        .distinct()
+                        .peek(System.out::println)
+                        .count();
+
+        System.out.println(totalColours);
+    }
+}
+
+class Car {
+    private final String model;
+    private final String[] colours;
+
+    public Car(String model, String[] colours) {
+        this.model = model;
+        this.colours = colours;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String[] getColours() {
+        return colours;
     }
 }
