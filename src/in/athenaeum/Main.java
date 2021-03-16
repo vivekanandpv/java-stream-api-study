649f48b001c447f1f9390ffd4e50deac0e3b106b
vivek <vivek@athenaeum.in>
2021-03-16 06:57:38 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -7,13 +7,60 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String[] args) {
-        List<String> names = List.of("Raman", "Einstein", "Rutherford", "Heisenberg", "Bohr");
+        List<ComplexNumber> complexNumbers =
+                List.of(
+                        new ComplexNumber(1, 1),
+                        new ComplexNumber(2, 1),
+                        new ComplexNumber(3, 2),
+                        new ComplexNumber(2, 2)
+                );
 
-        //  flatMap applies map to inner collection
-        //  it's a map that returns another stream explicitly
-        //  Arrays.stream() is a convenience method
-        names.stream()
-                .flatMap(name -> Arrays.stream(name.split("[aeiou]")))
-                .forEach(System.out::println);
+        //  A reducer is a pure function
+        //  In this first variant, reducer is applied iteratively
+        //  For example:
+        //  0   1   2   3   4    5   // original stream
+        //  0 + 1   // reducer returns 1
+        //      1 + 2   // reducer returns 3
+        //          3 + 3   // reducer returns 6
+        //              6 + 4   // reducer returns 10
+        //                  10 + 5  // reducer returns 15
+        //                      15  // no c2 from this point. result = 15
+
+        //  why Optional<T>?
+        Optional<ComplexNumber> resultOptional = complexNumbers.stream()
+                .reduce(
+                        (c1, c2) -> new ComplexNumber(
+                                c1.getReal() + c2.getReal(),
+                                c1.getImaginary() + c2.getImaginary()
+                        )
+                );
+
+        System.out.println(resultOptional);
+    }
+}
+
+class ComplexNumber {
+    private final double real;
+    private final double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    @Override
+    public String toString() {
+        return this.getReal() + " "
+                + (this.getImaginary() > 0 ? " + " + this.getImaginary() + "i" :
+                this.getImaginary() < 0 ? " - " + this.getImaginary() + "i" :
+                        "");
     }
 }
