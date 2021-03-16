e640d675bc5df364ab459faf62ae101b85e5bfba
vivek <vivek@athenaeum.in>
2021-03-16 12:58:36 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -8,67 +8,38 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
-        List<ComplexNumber> complexNumbers =
-                List.of(
-                        new ComplexNumber(1, 1),
-                        new ComplexNumber(2, 1),
-                        new ComplexNumber(3, 2),
-                        new ComplexNumber(2, 2)
-                );
+        List<User> userList = List.of(
+                new User("vivek", Set.of("admin", "manager")),
+                new User("arjun", Set.of("backup", "user")),
+                new User("asad", Set.of("developer")),
+                new User("bharat", Set.of("qa", "user"))
+        );
 
-        //  toMap requires a key extractor and a value extractor
-        Map<Integer, ComplexNumber> mapDump =
-                IntStream.range(0, complexNumbers.size())
-                .mapToObj(i -> new IndexedComplexNumber(i, complexNumbers.get(i)))  // map returns IntStream, hence mapToObj
-                .collect(
-                        Collectors.toMap(
-                                IndexedComplexNumber::getIndex,
-                                IndexedComplexNumber::getComplexNumber
-                        )
-                );
+        Map<String, Set<String>> userMap =
+                userList.stream()
+                        .collect(
+                                Collectors.toMap(
+                                        User::getUsername,
+                                        User::getRoles
+                                )
+                        );
     }
 }
 
-class IndexedComplexNumber {
-    private final int index;
-    private final ComplexNumber complexNumber;
+class User {
+    private final String username;
+    private final Set<String> roles;
 
-    public IndexedComplexNumber(int index, ComplexNumber complexNumber) {
-        this.index = index;
-        this.complexNumber = complexNumber;
+    public User(String username, Set<String> roles) {
+        this.username = username;
+        this.roles = roles;
     }
 
-    public int getIndex() {
-        return index;
+    public String getUsername() {
+        return username;
     }
 
-    public ComplexNumber getComplexNumber() {
-        return complexNumber;
-    }
-}
-
-class ComplexNumber {
-    private final double real;
-    private final double imaginary;
-
-    public ComplexNumber(double real, double imaginary) {
-        this.real = real;
-        this.imaginary = imaginary;
-    }
-
-    public double getReal() {
-        return real;
-    }
-
-    public double getImaginary() {
-        return imaginary;
-    }
-
-    @Override
-    public String toString() {
-        return this.getReal() + " "
-                + (this.getImaginary() > 0 ? " + " + this.getImaginary() + "i" :
-                this.getImaginary() < 0 ? " - " + this.getImaginary() + "i" :
-                        "");
+    public Set<String> getRoles() {
+        return roles;
     }
 }
