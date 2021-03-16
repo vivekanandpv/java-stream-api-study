e71fc32724a6d121a540f9d261b45eb421cadaa1
vivek <vivek@athenaeum.in>
2021-03-16 07:01:00 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -9,30 +9,23 @@ public class Main {
     public static void main(String[] args) {
         List<ComplexNumber> complexNumbers =
                 List.of(
-                        new ComplexNumber(1, 1),
-                        new ComplexNumber(2, 1),
-                        new ComplexNumber(3, 2),
-                        new ComplexNumber(2, 2)
+                        new ComplexNumber(1, 1)
                 );
 
-        //  A reducer is a pure function
-        //  In this first variant, reducer is applied iteratively
-        //  For example:
-        //  0   1   2   3   4    5   // original stream
-        //  0 + 1   // reducer returns 1
-        //      1 + 2   // reducer returns 3
-        //          3 + 3   // reducer returns 6
-        //              6 + 4   // reducer returns 10
-        //                  10 + 5  // reducer returns 15
-        //                      15  // no c2 from this point. result = 15
+        //  When there is only one element in the stream
+        //  that element is the result of the reduction
+        //  Processing is short-circuited. Reducer doesn't run
+        //  but the result is produced.
 
-        //  why Optional<T>?
         Optional<ComplexNumber> resultOptional = complexNumbers.stream()
                 .reduce(
-                        (c1, c2) -> new ComplexNumber(
-                                c1.getReal() + c2.getReal(),
-                                c1.getImaginary() + c2.getImaginary()
-                        )
+                        (c1, c2) -> {
+                            System.out.println("Reducer runs with " + c1 + " and " + c2);
+                            return new ComplexNumber(
+                                    c1.getReal() + c2.getReal(),
+                                    c1.getImaginary() + c2.getImaginary()
+                            );
+                        }
                 );
 
         System.out.println(resultOptional);
