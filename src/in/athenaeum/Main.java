51b6f055766d91b1be688d326cae9316df8a9ce9
vivek <vivek@athenaeum.in>
2021-05-25 14:53:51 +0530

    done

--- src/in/athenaeum/Main.java
+++ src/in/athenaeum/Main.java
@@ -1,19 +1,40 @@
 package in.athenaeum;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<String> names = List.of("Raman", "Einstein", "Rutherford", "Heisenberg", "Bohr");
+        List<User> users = new ArrayList<>();
+        users.add(new User("Pranav", List.of("Admin", "Manager", "User")));
+        users.add(new User("Asha", List.of("Finance", "Controller", "User")));
+        users.add(new User("Ramakant", List.of("Developer", "Lead", "User")));
 
         //  flatMap applies map to inner collection
         //  it's a map that returns another stream explicitly
-        //  Arrays.stream() is a convenience method
-        names.stream()
-                .flatMap(name -> Arrays.stream(name.split("[aeiou]")))
+        users.stream()
+                .flatMap(u -> u.getRoles().stream())
                 .forEach(System.out::println);
+    }
+}
+
+
+class User {
+    private String username;
+    private List<String> roles;
+
+    public User(String username, List<String> roles) {
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
